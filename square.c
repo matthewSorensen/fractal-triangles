b848e1e5756bb0419a873c7a6695efd57269c518
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2014-11-09 14:47:12 -0800

    Clean up dealing with stl headers

--- square.c
+++ square.c
@@ -1,28 +1,43 @@
 #include<stdio.h>
 #include <string.h>
-
+#include <stdint.h>
 
 unsigned int ones(int x){
   unsigned int acc = 0;
   unsigned int bit = 1;
-
+  
   while(x > 0){
     int q = x / 3;
     int r = x - 3*q;
-
+    
     if(r == 1) acc |= bit;
     bit = bit << 1;
     x = q;
   }
-
+  
   return acc;
 }
 
-int rotations[18] = {0, 1, 2,   1, 0, 2,   2, 1, 0};
-float delta[18] = {0.0, 0, 1.0,    0.0, 0, 0,   0.0, 1.0, 0,
-		   0.0, 0, 1.0,    0.0, 1.0, 0,  0.0, 1.0, 1.0};
+int in_set(int ones, int x){
+  while(x > 0){
+    int q = x / 3;
+    int r = x - q * 3;
+    
+    if(r == 1 && (ones & 1))
+      return 0;
+    ones = ones >> 1;
+    x = q;
+  }
+  return 1;
+}
+
+
+
+int rotations[18] = {0,1,2,   1,0,2,   2,1,0};
+float delta[18] = {0.0, 0.0, 1.0,    0.0, 0.0, 0.0,    0.0, 1.0, 0.0,
+		   0.0, 0.0, 1.0,    0.0, 1.0, 0.0,  0.0, 1.0, 1.0};
 float normal[3] = {0,0,0};
-int normal_rot[18] = {1,0,2, 0,1,2,  0,1,2, 1,0,2, 0,1,2, 1,0,2 };
+int normal_rot[18] = {1,0,2, 0,1,2,  0,1,2,1,0,2,  0,1,2,1,0,2 };
 
 void square(int i, int j, int k, int n, FILE* fp, int dir){
   float half = ((float) n) / -2.0;
@@ -49,60 +64,49 @@ void square(int i, int j, int k, int n, FILE* fp, int dir){
   }
 }
 
-int in_set(int ones, int x){
-  while(x > 0){
-    int q = x / 3;
-    int r = x - q * 3;
-    
-    if(r == 1 && (ones & 1))
-      return 0;
-    ones = ones >> 1;
-    x = q;
-  }
-  return 1;
-}
+typedef struct {
+  char head[80];
+  uint32_t count;
+} stl_header;
 
 int main(int argc, char** argv){
   int n = 27;
-  // Now we take each cube and generate triangles...
   FILE* fp = fopen("test.stl","w");
-  char header[84];
-  memset(header, 0, 84);
-  int count = 0;
-  fwrite(&header,sizeof(char), 84, fp);
+  
+  // Write a dummy stl header out, just to occupy the correct 
+  // amount of space at the start of the file.
+  stl_header head;
+  memset(&head, 0, sizeof(stl_header));
+  fwrite(&head, sizeof(stl_header), 1, fp);
+  
   for(int j = 0; j < n; j++){ 
     int zones = ones(j);
-
     for(int k = 0; k < n; k++){
       int yones = ones(k);
-
       if(zones & yones) 
 	continue;
-
       yones |= zones;
-      
       int status = 0; 
       for(int i = 0; i < n; i++){
 	int next = in_set(yones, i);
 	if(status ^  next){
 	  square(i,j,k,n,fp, status);
-	  count += 6;
+	  head.count += 6;
 	}
 	status = next;
       }
-
       if(status){
 	square(n,j,k,n,fp, status);
-	count += 6;
+	head.count += 6;
       }
-
     }
   }
 
-  fseek(fp,80,SEEK_SET);
-  fwrite(&count, sizeof(int), 1, fp);
+  // Seek to the begining of the file and write out a correct header
+  fseek(fp,0,SEEK_SET);
+  fwrite(&head, sizeof(stl_header), 1, fp);
   fclose(fp);
-
+  
   return 0;
 }
 
