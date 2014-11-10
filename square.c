7a7f1506aefbf3fd4ef3cd91adf6239f71e9b371
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2014-11-10 10:16:21 -0800

    we'll call that done for this project

--- square.c
+++ square.c
@@ -32,32 +32,44 @@ int in_set(int ones, int x){
 }
 
 
-
-int rotations[18] = {0,1,2,   1,0,2,   2,1,0};
+// Permutation of coordinates to implement spatial rotation
+int rotations[9] = {0,1,2,   1,0,2,   2,1,0};
+// Coordinate offsets to generate two triangles
 float delta[18] = {0.0, 0.0, 1.0,    0.0, 0.0, 0.0,    0.0, 1.0, 0.0,
 		   0.0, 0.0, 1.0,    0.0, 1.0, 0.0,  0.0, 1.0, 1.0};
+// Dummy surface normal vector
 float normal[3] = {0,0,0};
+// Permutation of vertexes to keep normals constant
 int normal_rot[18] = {1,0,2, 0,1,2,  0,1,2,1,0,2,  0,1,2,1,0,2 };
 
 void square(int i, int j, int k, int n, FILE* fp, int dir){
   float half = ((float) n) / -2.0;
   float vect[3] = {half + (float) i, half + (float) j, half + (float) k};
   if(dir){dir = 3;}
-  // Print half of the thingy
+  // We need to build three copies of each square - one parallel with yz, one with xz, and the other with xy.
   for(int rot = 0; rot < 9; rot += 3){
+    // For each square, we need to build two triangles - go through the triangle-building process twice,
+    // each with a different half of the offsets array.
     for(int half = 0; half < 18; half += 9){
-      // Three zeroes for the normal vector
+      // For each triangle, we emit three zeroes for the normal vector - the stl convention is
+      // that the normal is actually infered from triangle vertex order anyways.
       fwrite(&normal, sizeof(float), 3, fp);
+      // For each triangle, we need to create three vertexes
       for(int offset = 0; offset < 3; offset++){
+	// The order with which we create  them (and thus the normal vector orientation)
+	// depends on type of transition creating the square (dir), and the final
+	// orientation of the triangle in space (rot).
 	int off = 3 * normal_rot[offset + dir + 2*rot];
-	// The three vector components, suitably permuted and offset
+	// Now we can create the three components for a vertex:
 	for(int ii = 0; ii < 3; ii++){
+	  // First apply the rotation matrix to figure out what component we're using
 	  int rotated = rotations[rot + ii];
+	  // Then select the component and compute the proper offset
 	  float component = vect[rotated]+delta[half + off + rotated];
 	  fwrite(&component, sizeof(float), 1, fp);
 	}
       }
-      // the attribute thingy
+      // the facet attribute thingy
       fputc(0, fp);
       fputc(0, fp);
     }
